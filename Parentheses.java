cbca3ed58aa49aa682c57d9fe8f39db1ce444943
Derin-WJ <derinsoysalis2009@gmail.com>
2025-11-10 16:03:14 +0000

    1st day

--- Parentheses.java
+++ Parentheses.java
@@ -1,1 +1,21 @@
+//Derin Soysal Period 4
+//11/10/25
+//This program will check a string given to the program consisting of parentheses, square brackets,
+//and curly brackets to determine if it is a valid string
+public class Parentheses{
+//This is the main method where the methods will be called to find the validity of the string
+    public static void main(String[] args){
+        checker("({})");
+    }
 
+    //  Pre-condition: String must only consist of parentheses, square brackets, and curly brackets
+    //  Post-condition: It will output text saying if the string is valid or not
+    public static void checker(String s){
+        for(int i = 0; i < s.length(); i++){
+            if(s.substring(i, i+1).equals())
+        }
+
+
+
+    }
+}
