a5fd48fd0ed1a013c318e80bfadaeec9b024ddcd
Derin-WJ <derinsoysalis2009@gmail.com>
2025-11-12 01:04:01 +0000

    Final

--- Parentheses.java
+++ Parentheses.java
@@ -5,20 +5,32 @@
 public class Parentheses{
 //This is the main method where the methods will be called to find the validity of the string
     public static void main(String[] args){
-        checker("{()}");
+        checker("{}()");
+
     }
 
-    //  Pre-condition: String must only consist of parentheses, square brackets, and curly brackets
+    //  Pre-condition: String must only consist of parentheses, square brackets, and curly brackets. String cannot be empty.
     //  Post-condition: It will output text saying if the string is valid or not
     public static void checker(String s){
         int i = 0;
         String copy = s;
         String temp = "";
-        while(copy.length() > 0){
-            while(i<copy.length()){
+        String result = "";
+
+            while(i < copy.length()){
 
                 //find first closing simbol
-                if(copy.substring(i, i+1).equals(")") || copy.substring(i, i+1).equals("}") || copy.substring(i, i+1).equals("]")){
+                if(copy.substring(0, 1).equals(")") 
+                || copy.substring(0, 1).equals("}") 
+                || copy.substring(0, 1).equals("]")){
+                    //if the first symbol is a closing symbol, the string is invalid
+                    result = "String is not valid";
+                    System.out.println(result);
+                    return;
+
+                } else if(copy.substring(i, i+1).equals(")") 
+                || copy.substring(i, i+1).equals("}") 
+                || copy.substring(i, i+1).equals("]")){
                     //temp is supposed to equal the last open bracket
                     temp = copy.substring(i-1, i);
                     //found closing symbol at i
@@ -27,19 +39,22 @@ public class Parentheses{
                     || (temp.equals("{") && copy.substring(i, i+1).equals("}"))
                     ||(temp.equals("[") && copy.substring(i, i+1).equals("]"))){
                         //change copy to equal everything except what's at index i-1 and i and set i back to 0
-                        copy = copy.substring(0, i) + copy.substring(i+2, copy.length());
+                        copy = copy.substring(0, i-1) + copy.substring(i+1, copy.length());
+                        i = -1;
                     }
+                    
                 }
                 i++;
             }
-        }
-    if(copy.length() == 0){
-        System.out.println("String is valid");
-    } else{
-       System.out.println("String is not valid"); 
+
+    if(copy.length() == 0 && !"String is not valid".equals(result)){
+        result = "String is valid";
+       System.out.println(result);
+    } 
+    if(copy.length() != 0 && !"String is not valid".equals(result)){
+       result = "String is not valid";
+       System.out.println(result);
     }
-
-
 
     }
 }
